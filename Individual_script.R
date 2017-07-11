0e18ce85428986f6770b88159ea64ce802686ef4
Tyler Amano-Smerling <tyleramano-smerling@MacBook-Pro-7.local>
2017-07-11 11:39:11 -0700

    Save script; currently looks at a single window and analyzes # of SNPs mismatching, runs through and outputs smallest distance, loc of smallest distance, and pop of smallest distance

---
+++ Individual_script.R
@@ -0,0 +1,44 @@
+#For ped file dealing with letters
+DATA=read.table("1.ped",row.names=2)
+K=dim(DATA)
+X=DATA[8,]
+WIN=500
+#step=150
+count=0
+count1=0
+d=0
+index=0
+smallest = 1
+loc_smallest = 0
+pop_smallest = ""
+for(start in index*WIN+6:(length(X)-WIN)){#over windiws
+  d_vector=c()
+  #over row
+  for(i in 1:K[1]){#over database individuals
+    #i = 2
+    count=WIN #count = total no. of SNPs in window
+    count1=0 #clear value for mismatches
+    for(j in start:(start+WIN)){#within the window
+      if(X[j][[1]]!=DATA[i,j]){
+        count1=count1+1 #increase no. of mismatches if data points aren't equal
+      }
+    }
+    d=count1/count
+    d_vector=c(d_vector,d)
+  }
+  smallest = min(d_vector)
+  for(x in 1:length(d_vector)){
+    if(d_vector[x] == smallest){
+      loc_smallest = x
+      pop_smallest = as.character(DATA[x,1][[1]])
+      break;
+    }
+  }
+  if((index*WIN) > length(X)){
+    break
+  }
+}#end go over reference
+print(d_vector)
+
+
+
