4fe015d090d2b5d21ecb3b244dad7a366bc0945c
Tyler Amano-Smerling <tyleramano-smerling@MacBook-Pro-7.local>
2017-07-13 12:02:48 -0700

    Work out kinks with creating new baby. Currently pulls SNPs from one chinese and one russian stv sample.

--- Downloads/Bind_people.R
+++ Downloads/Bind_people.R
@@ -1,49 +1,54 @@
 DATA=read.table("1.ped",row.names=2)
-K=dim(DATA)
-baby = c()
+BABY = c()
 Break = c()
+vv=c(rep("not used",K[1]))
 used_column = c(rep("not used",K[1]))
 dc=cbind(DATA,used_column)
-#while(TRUE){
-  all_used = FALSE
-  p1 = round(runif(1, 1,K[1]))
-  p2 = round(runif(1, 1,K[1]))
-  while(identical(dc[p1,K[2]],"used")){
-    p1 = runif(1, 1:K[1])
-  }
-  while(identical(dc[p2,K[2]],"used")){
-    p2 = runif(1, 1:K[1])
-  }
-  while(p1 == p2){
-    p2 = runif(1, 1:K[1])
-  }
-  #replace(dc,dc[p1,K[2]],"used")
+K=dim(dc)
+BABY_matrix=c()
+index=0
+parents= c()
+pop1_SNP=0
+pop2_SNP=0
+all_used = FALSE
+#while(all_used == FALSE){
+  p1 = #round(runif(1, 1,K[1]))
+  p2 = #round(runif(1, 1,K[1]))
+  #while(identical(dc[p1,K[2]],"used")){
+  #  p1 = runif(1, 1:K[1])
+  #}
+  #while(identical(dc[p2,K[2]],"used")){
+  #  p2 = runif(1, 1:K[1])
+  #}
+  #while(p1 == p2){
+  #  p2 = runif(1, 1:K[1])
+  #}
   vv[p1]='used'
   dc$used_column=vv
   dc[p1,K[2]]
   vv[p2]='used'
   dc$used_column=vv
   dc[p2,K[2]]
-  person1 <- c(dc[p1, K[2]])
-  person2 <- c(dc[p2, K[2]])
-  Break = sort(round(runif(100, 6, K[2]))) 
-  baby = person1[1: (Break[1] - 1)]
-  for(x in 1: (length(Break) - 1)){
+  person1 <- dc['4257735763_A',] #dc[p1,1:(K[2]-1)]
+  person2 <- dc['4257735504_A',] #dc[p2,1:(K[2]-1)]
+  Break = sort(round(runif(100, 7, K[2]))) 
+  BABY=person1# c(BABY,person1[6:(Break[1] - 1)])
+  parents= rep(dc['4257735763_A',1],length(BABY))
+  for(x in 1: (length(Break) - 2)){
     if(x %% 2 == 0){
-      baby <- c(baby, person1[Break[x]: Break[x+1]])
-    }else{
-      baby <- c(baby, person2[Break[x]: Break[x+1]])
+      BABY[Break[x]:Break[x+1]]=person2[Break[x]: Break[x+1]]
+      parents[Break[x]:Break[x+1]]=dc['4257735504_A',1]
     }
   }
-  for(index in 1:K[1]){
-    if(identical(dc[index, K[2]],"not_used")){
-      break
-    } else {
-      all_used = TRUE
-    }
+  BABY_matrix=rbind(BABY_matrix,BABY)
+  for(p in 1:length(parents)){
+    if(parents[p]==dc['4257735763_A',1]){pop1_SNP=pop1_SNP+1}
+    if(parents[p]==dc['4257735504_A',1]){pop2_SNP=pop2_SNP+1}
   }
-  #if(all_used == TRUE){
-   # break
-  #}
+  names(BABY) <- parents
+  index=index+2
+  if(index == K[1]){
+    all_used = TRUE
+  }
 #}
   
