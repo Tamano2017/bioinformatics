9aaa5385043edd7bd67bd6cbf98e8f45892f001d
Tyler Amano-Smerling <tyleramano-smerling@MacBook-Pro-7.local>
2017-07-16 18:39:51 -0700

    most recent version of script

--- Individual_script.R
+++ Individual_script.R
@@ -6,8 +6,8 @@ predicted_pop=c()
 false=0
 K=dim(DATA)
 pos=0
-WIN=100
-step=100
+WIN=10
+step=10
 count=0
 count1=0
 d=0
@@ -17,27 +17,27 @@ loc_smallest = 0
 pop_smallest = ""
 pop_vector=c()
 #for(pop in 1:K[1]){
-  d_vector=c()
-  X = BABY[1,]
+X = DATA[10,]
 #for(start in 6:(length(X)-WIN)){#over windiws
   for(start in seq(from=6, to=(length(X)-WIN), by=step)){
+  d_vector=c()
   #over row
   for(i in 1:K[1]){#over database individuals
     #i = 2
     count=WIN #count = total no. of SNPs in window
     count1=0 #clear value for mismatches
     for(j in start:(start+WIN)){#within the window
-      if(identical(as.character(X[j][[1]]),as.character(DATA[i,j]))){
-        count1=count1+1 #increase no. of mismatches if data points aren't equal
+      count1=0
+      if(as.character(X[j][[1]])==as.character(DATA[i,j])){
+        count1=count1+1 #increase no. of matches if data points aren't equal
       }
     }
-    d=(count-count1)/count
+    d=count1/count
     d_vector=c(d_vector,d)
   }
-  smallest = min(d_vector)
-  for(x in 1:length(d_vector)){
+  smallest = max(d_vector)
+  for(x in 1:dim(DATA)[1]){
     if(identical(d_vector[x],smallest)){
-      print("poop")
       loc_smallest = x
       pop_smallest = as.character(DATA[x,1][[1]])
       break;
